82c82ea034486d16834330b2a6ec1a8333fa5d23
Simon Ho <simonho213@gmail.com>
2019-10-11 16:16:01 -0700

    Comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rise of Kingdoms Talent Tree
+
+Took inspiration from https://github.com/Nkmol/RoC-talent-tree
--- src/Tree.js
+++ src/Tree.js
@@ -17,36 +17,6 @@ class Tree extends React.Component {
 
   render() {
     return (
-      //FIXME: reactstrap grid is very slow compared to regular bootstrap
-      //   <div id="tree">
-      //   <div className="container">
-      //     <div className="row align-items-center sub-tree">
-      //       <div id="tree-red" className="col sub-tree-col">
-      //         {this.props.redTree}
-      //       </div>
-      //     </div>
-
-      //     <div className="row align-items-center">
-      //       <div id="tree-commander" className="col">
-      //         <img
-      //           src={`images/commanders/${this.props.commander}.png`}
-      //           alt={this.props.commander}
-      //         ></img>
-      //         <div id="commander-name">{this.props.commander}</div>
-      //       </div>
-      //     </div>
-
-      //     <div className="row align-items-center sub-tree">
-      //       <div id="tree-yellow" className="col sub-tree-col">
-      //         {this.props.yellowTree}
-      //       </div>
-      //       <div id="tree-blue" className="col sub-tree-col">
-      //         {this.props.blueTree}
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-
       <div id="tree">
         <Container>
           <Row className="align-items-center sub-tree">
