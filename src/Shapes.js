8083f4e4b02312173c81ce81407b6a95e5cc0193
Simon Ho <simonho213@gmail.com>
2019-10-13 00:43:59 -0700

    Refactor commander hexagon

--- src/Shapes.js
+++ src/Shapes.js
@@ -4,11 +4,21 @@ import React from 'react';
 class HexagonCommander extends React.Component {
   render() {
     return (
-      <div>
+      <div id="tree-commander" className="col">
         <div className="hexagon-talent-wrapper">
           <div className="hexagon-talent-red"></div>
           <div className="hexagon-talent-yellow"></div>
           <div className="hexagon-talent-blue"></div>
+        </div>
+
+        <div className="hexagon-label hexagon-label-red">
+          {this.props.getTreeName('red')}
+        </div>
+        <div className="hexagon-label hexagon-label-yellow">
+          {this.props.getTreeName('yellow')}
+        </div>
+        <div className="hexagon-label hexagon-label-blue">
+          {this.props.getTreeName('blue')}
         </div>
 
         <div className="hexagon-commander-wrapper">
--- src/Tree.js
+++ src/Tree.js
@@ -4,6 +4,11 @@ import commanders from './data/commanders.json';
 
 // Talent tree container
 class Tree extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getTreeName = this.getTreeName.bind(this);
+  }
+
   getTreeName(color) {
     const commander = commanders[this.props.commander];
     if (commander) {
@@ -15,32 +20,28 @@ class Tree extends React.Component {
     return (
       <div id="tree">
         <div className="container d-flex flex-column">
-
           <div className="row align-items-center flex-fill">
             <div className="col"></div>
-            <div id="tree-red" className="col-6 tree-col">a</div>
+            <div id="tree-red" className="col-6 tree-col">
+              a
+            </div>
             <div className="col"></div>
           </div>
 
           <div className="row align-items-center">
-            <div id="tree-commander" className="col">
-              <HexagonCommander commander={this.props.commander} />
-
-              <div className="hexagon-label hexagon-label-red">
-                {this.getTreeName('red')}
-              </div>
-              <div className="hexagon-label hexagon-label-yellow">
-                {this.getTreeName('yellow')}
-              </div>
-              <div className="hexagon-label hexagon-label-blue">
-                {this.getTreeName('blue')}
-              </div>
-            </div>
+            <HexagonCommander
+              commander={this.props.commander}
+              getTreeName={this.getTreeName}
+            />
           </div>
 
           <div className="row align-items-center flex-fill">
-            <div id="tree-yellow" className="col tree-col">b</div>
-            <div id="tree-blue" className="col tree-col">c</div>
+            <div id="tree-yellow" className="col tree-col">
+              b
+            </div>
+            <div id="tree-blue" className="col tree-col">
+              c
+            </div>
           </div>
         </div>
       </div>
