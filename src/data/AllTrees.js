37e55d1622b9a0d571464185fe14f1d1dbc18d75
Simon Ho <simonho213@gmail.com>
2020-01-03 18:44:04 -0800

    Add Alex talents

--- src/data/AllTrees.js
+++ src/data/AllTrees.js
@@ -1,6 +1,7 @@
 // Utility to map data files to object
 function loadTreeData(version) {
   const treeData = {
+    Attack: require(`./v${version}/Attack.json`),
     Archer: require(`./v${version}/Archer.json`),
     Defense: require(`./v${version}/Defense.json`),
     Garrison: require(`./v${version}/Garrison.json`),
@@ -8,7 +9,8 @@ function loadTreeData(version) {
     Leadership: require(`./v${version}/Leadership.json`),
     Peacekeeping: require(`./v${version}/Peacekeeping.json`),
     Skill: require(`./v${version}/Skill.json`),
-    Support: require(`./v${version}/Support.json`)
+    Support: require(`./v${version}/Support.json`),
+    Versatility: require(`./v${version}/Versatility.json`)
   };
 
   if (process.env.NODE_ENV === 'development') {
