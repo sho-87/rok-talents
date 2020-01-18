9b6381332e5c74e6236d5c5b957f398fe0d43461
Simon Ho <simonho213@gmail.com>
2020-01-17 18:41:59 -0800

    Combine data tests

--- src/__tests__/AllTrees.test.js
+++ src/__tests__/AllTrees.test.js
@@ -1,8 +1,36 @@
 import loadTreeData from '../data/AllTrees';
-import { isMultidimensional } from '../utils';
+import { isMultidimensional, arraysEqual } from '../utils';
 import { dataVersion } from '../../package.json';
 
-test('all multidimensional talent values are same length', () => {
+const treeData = loadTreeData(dataVersion);
+
+test('All tree data is sufficiently long', () => {
+  const isLongEnough = Object.keys(treeData).every(tree => {
+    return Object.keys(treeData[tree]).length > 10;
+  });
+
+  expect(isLongEnough).toEqual(true);
+});
+
+test('No repeat IDs', () => {
+  const noRepeats = Object.keys(treeData).every(tree => {
+    const correctIDs = Array.from(
+      { length: Object.keys(treeData[tree]).length },
+      (v, i) => i + 1
+    );
+
+    const ids = Object.keys(treeData[tree]).map(function(x) {
+      return parseInt(x);
+    });
+
+    return arraysEqual(ids, correctIDs);
+  });
+
+  expect(noRepeats).toEqual(true);
+});
+
+
+test('All multidimensional talent values are same length', () => {
   let treeData = loadTreeData(dataVersion);
   let allSameLength = true;
 
