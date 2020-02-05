b5e955778ff48d7c5f8ccf1f60ba09d1454de57c
Simon Ho <simonho213@gmail.com>
2020-02-05 00:27:02 -0800

    Updates

---
+++ precache-manifest.46fd0432e55f39147242ff0d31c3baec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c16ef4f6ceda94b2d64de64bf409b7",
+    "url": "/rok-talents/index.html"
+  },
+  {
+    "revision": "58174fe45b184bae2bdc",
+    "url": "/rok-talents/static/css/4.f8dd722f.chunk.css"
+  },
+  {
+    "revision": "7ebe8c12fd1b85676f42",
+    "url": "/rok-talents/static/css/main.498d201d.chunk.css"
+  },
+  {
+    "revision": "6f86b09535e79929dd0a",
+    "url": "/rok-talents/static/js/2.f0cd7321.chunk.js"
+  },
+  {
+    "revision": "3b8525585488aa027a3f5c99e7c3fb16",
+    "url": "/rok-talents/static/js/2.f0cd7321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda78cfbb3fec3cf26bf",
+    "url": "/rok-talents/static/js/3.27965937.chunk.js"
+  },
+  {
+    "revision": "58174fe45b184bae2bdc",
+    "url": "/rok-talents/static/js/4.b896f979.chunk.js"
+  },
+  {
+    "revision": "7ebe8c12fd1b85676f42",
+    "url": "/rok-talents/static/js/main.2a06de63.chunk.js"
+  },
+  {
+    "revision": "90ba450464bf211a4b75",
+    "url": "/rok-talents/static/js/runtime-main.1bdb05da.js"
+  }
+]);
