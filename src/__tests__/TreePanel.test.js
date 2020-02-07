52e73f9549e6d2f851deb806d4b2951493a39353
Simon Ho <simonho213@gmail.com>
2020-02-07 00:47:16 -0800

    Fix tests

--- src/__tests__/TreePanel.test.js
+++ src/__tests__/TreePanel.test.js
@@ -4,6 +4,20 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TreePanel from '../TreePanel';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+});
+
 let props;
 beforeEach(() => {
   props = { red: [], yellow: [], blue: [] };
