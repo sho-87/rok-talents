0ff0035dc66dedd0306a5f4366b721eb4b7201e4
sho-87 <sho-87@users.noreply.github.com>
2020-02-10 07:22:11 +0000

    Deploying to gh-pages from  - e9a1e829597b3005b5179dfd2bbe21f3c57e51ff 🚀

---
+++ precache-manifest.d4c5deb1620fba6d20f11f4c76d8bc86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7781432696977200888b2c34d6362e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf32db07d148d6cf5dea",
+    "url": "/static/css/4.be2aeaac.chunk.css"
+  },
+  {
+    "revision": "f52139dc8c5227729c36",
+    "url": "/static/css/main.a26b9c50.chunk.css"
+  },
+  {
+    "revision": "c8ffc6d8fd96238267a4",
+    "url": "/static/js/2.e81fa5c7.chunk.js"
+  },
+  {
+    "revision": "3b8525585488aa027a3f5c99e7c3fb16",
+    "url": "/static/js/2.e81fa5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06ab4581d37c56349af",
+    "url": "/static/js/3.5ac3e9a6.chunk.js"
+  },
+  {
+    "revision": "bf32db07d148d6cf5dea",
+    "url": "/static/js/4.0c1991aa.chunk.js"
+  },
+  {
+    "revision": "f52139dc8c5227729c36",
+    "url": "/static/js/main.2fdbb63d.chunk.js"
+  },
+  {
+    "revision": "27747fee2c6125c1d2d3",
+    "url": "/static/js/runtime-main.e421751f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7dae2ab74fedddc3f0a846fffb3f9d64.js"
+  "/precache-manifest.d4c5deb1620fba6d20f11f4c76d8bc86.js"
 );
 
 self.addEventListener('message', (event) => {
