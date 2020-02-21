a4eb9823458f041125a8aa03a76dc22d1c8f49f4
Simon Ho <simonho213@gmail.com>
2020-02-21 00:00:38 -0800

    Disable content embedding

--- src/index.js
+++ src/index.js
@@ -4,13 +4,29 @@ import ReactGA from 'react-ga';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-ReactGA.initialize('UA-114296077-2');
-ReactGA.pageview('/');
+if (window.self === window.top) {
+  ReactGA.initialize('UA-114296077-2');
+  ReactGA.pageview('/');
 
-const url = window.location.search;
-ReactDOM.render(<App url={url} />, document.getElementById('root'));
+  const url = window.location.search;
+  ReactDOM.render(<App url={url} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+} else {
+  ReactDOM.render(
+    <div>
+      Content embedding is disabled. Please visit the original website at{' '}
+      <a
+        href="https://www.roktalents.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        roktalents.com
+      </a>
+    </div>,
+    document.getElementById('root')
+  );
+}
