1fe95a58f7e00e04208eca459b7a9cae7fea5e64
sho-87 <sho-87@users.noreply.github.com>
2020-03-03 06:25:43 +0000

    Deploying to gh-pages from  - 0c6623c0cd92ac791f9fc4d170f5019c2d4b31db 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc94cc04b2642d2236eabf1d122ffdb4.js"
+  "/precache-manifest.6755ea5cc1f1c9b514db917c340dabe2.js"
 );
 
 self.addEventListener('message', (event) => {
